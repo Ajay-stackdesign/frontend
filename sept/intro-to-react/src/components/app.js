568114e148cb4ab63c0224ecfdabc4d215399e8e
Ajay Sahani <ajaygsahani968@gmail.com>
2021-10-07 23:43:55 +0530

    commit

---
+++ sept/intro-to-react/src/components/app.js
@@ -0,0 +1,16 @@
+import React from "react"
+import CountButton from "./countButton"
+import SearchBar from "./Search/SearchBar"
+
+const App =() => {
+    return (
+      <div>
+          <SearchBar />
+          <CountButton incrementBy={2}/>
+          <CountButton incrementBy={5}/>
+
+      </div>
+    )
+}
+
+export default App
